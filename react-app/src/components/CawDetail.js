bb362ea7e8b91c8104b09a7959edbe993d7e1ab6
Brian8771 <brian8771@gmail.com>
2022-09-02 16:13:28 -0500

    fixing refresh problem

--- react-app/src/components/CawDetail.js
+++ react-app/src/components/CawDetail.js
@@ -32,58 +32,63 @@ const PostDetail = () => {
 
     return (
         <div className='homePageContainer'>
-            <div>
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', border: 'black .5px solid' }}>
-                    <NavLink style={{ textDecoration: 'none' }} to='/'>
-                        <img style={{ height: '20px', width: '20px' }} src={backArrow} alt='backarrow' />
-                    </NavLink>
-                    <h2 style={{ marginLeft: '20px', color: 'black' }}>Thread</h2>
-                </div>
-                {caw &&
-                    <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '10rem', borderBottom: 'black .5px solid', borderLeft: 'black .5px solid', borderRight: 'black .5px solid', }}>
-                        <div style={{ display: 'flex', flexDirection: 'row' }}>
-                            <img style={{ height: '48px', width: '48px', borderRadius: '50%', padding: '5px 10px' }} src={caw.user.profileImage} alt='profilePic' />
-                            <div className='test' style={{ flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
-                                <p className='pTag'>{caw.user.username} <span style={{ color: 'gray' }}>@{caw.user.username}</span></p>
-                            </div>
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '6rem' }}>
-                            <div style={{ display: 'flex', width: '80%', padding: '10px' }}>
-                                <p className='pTag' >{caw.caw}</p>
-                            </div>
-                            <div style={{ display: 'flex', flexDirection: 'row', width: '20%', padding: '6px' }}>
-                                {caw.user.id === user.id && <EditFormModal setShowModal={setEditModal} caw={caw.id} />}
-                                {caw.user.id === user.id && <button style={{ backgroundColor: 'black', padding: '0', margin: '0', height: '25%', width: '100%', borderRadius: '40px' }} onClick={() => delete_caw(caw.id)}>Delete</button>}
-                            </div>
-                        </div>
+            {isLoaded &&
+                <div>
 
+                    <div>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', border: 'black .5px solid' }}>
+                            <NavLink style={{ textDecoration: 'none' }} to='/'>
+                                <img style={{ height: '20px', width: '20px' }} src={backArrow} alt='backarrow' />
+                            </NavLink>
+                            <h2 style={{ marginLeft: '20px', color: 'black' }}>Thread</h2>
+                        </div>
+                        {caw &&
+                            <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '10rem', borderBottom: 'black .5px solid', borderLeft: 'black .5px solid', borderRight: 'black .5px solid', }}>
+                                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                    <img style={{ height: '48px', width: '48px', borderRadius: '50%', padding: '5px 10px' }} src={caw.user.profileImage} alt='profilePic' />
+                                    <div className='test' style={{ flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+                                        <p className='pTag'>{caw.user.username} <span style={{ color: 'gray' }}>@{caw.user.username}</span></p>
+                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '6rem' }}>
+                                    <div style={{ display: 'flex', width: '80%', padding: '10px' }}>
+                                        <p className='pTag' >{caw.caw}</p>
+                                    </div>
+                                    <div style={{ display: 'flex', flexDirection: 'row', width: '20%', padding: '6px' }}>
+                                        {caw.user.id === user.id && <EditFormModal setShowModal={setEditModal} caw={caw.id} />}
+                                        {caw.user.id === user.id && <button style={{ backgroundColor: 'black', padding: '0', margin: '0', height: '25%', width: '100%', borderRadius: '40px' }} onClick={() => delete_caw(caw.id)}>Delete</button>}
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        }
                     </div>
+                    <div>
+                        <CreateComment setIsLoaded={setIsLoaded} />
+                    </div>
+                    <div>
+                        <div style={{ display: 'flex', flexDirection: 'column-reverse' }}>
+                            {comments && isLoaded &&
+                                comments.map(comment => {
+                                    return <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', borderBottom: 'black .5px solid', padding: '10px 10px', borderLeft: 'black .5px solid', borderRight: 'black .5px solid' }}>
+                                        <div>
+                                            <img style={{ height: '48px', width: '48px', borderRadius: '50%', padding: '5px 10px' }} src={comment.user.profileImage} alt='profilePic' />
+                                        </div>
+                                        <div className='test' style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                                            <NavLink style={{ textDecoration: 'none' }} to={`/${caw.id}`}>
+                                                <p className='pTag'>{comment.user.username} <span style={{ color: 'gray' }}>Replying to @{caw.user.username}</span></p>
+                                            </NavLink>
+                                            <p className='pTag' >{comment.data}</p>
+                                        </div>
 
-                }
-            </div>
-            <div>
-                <CreateComment setIsLoaded={setIsLoaded} />
-            </div>
-            <div>
-                <div style={{ display: 'flex', flexDirection: 'column-reverse' }}>
-                    {comments && isLoaded &&
-                        comments.map(comment => {
-                            return <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', borderBottom: 'black .5px solid', padding: '10px 10px', borderLeft: 'black .5px solid', borderRight: 'black .5px solid' }}>
-                                <div>
-                                    <img style={{ height: '48px', width: '48px', borderRadius: '50%', padding: '5px 10px' }} src={comment.user.profileImage} alt='profilePic' />
-                                </div>
-                                <div className='test' style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
-                                    <NavLink style={{ textDecoration: 'none' }} to={`/${caw.id}`}>
-                                        <p className='pTag'>{comment.user.username} <span style={{ color: 'gray' }}>Replying to @{caw.user.username}</span></p>
-                                    </NavLink>
-                                    <p className='pTag' >{comment.data}</p>
-                                </div>
-
-                            </div>
-                        })
-                    }
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
 
 
