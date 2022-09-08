f49f901b535c8b63f640061dddfd325aaaf26b34
Brian8771 <brian8771@gmail.com>
2022-09-07 19:08:30 -0500

    added more seeder files

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,19 +6,19 @@ def seed_users():
     demo = User(
         username='Demo1234',first_name='Demo',last_name='User', email='demo@aa.io', password='password', profile_image='https://cdn-icons-png.flaticon.com/512/3135/3135715.png', header_image='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Purple_website.svg/640px-Purple_website.svg.png', bio='Im just a demo user :)')
     marnie = User(
-        username='Marnie23',first_name='Marnie',last_name='User', email='marnie@aa.io', password='password1')
+        username='Marnie23',first_name='Marnie',last_name='User', email='marnie@aa.io', password='password1', profile_image='https://cdn-icons-png.flaticon.com/128/2920/2920072.png')
     bobbie = User(
-        username='BobbieThatDudes',first_name='Bobbie',last_name='User', email='bobbie@aa.io', password='password2')
+        username='BobbieThatDudes',first_name='Bobbie',last_name='User', email='bobbie@aa.io', password='password2', profile_image='https://cdn-icons-png.flaticon.com/128/3011/3011270.png')
     brian = User(
-        username='Brian8771',first_name='Brian',last_name='Aguilar', email='brian8771@aa.io', password='password3')
+        username='Brian8771',first_name='Brian',last_name='Aguilar', email='brian8771@aa.io', password='password3', profile_image='https://cdn-icons-png.flaticon.com/128/236/236831.png')
     test = User(
-        username='TestUser123',first_name='Test',last_name='Again', email='testing@aa.io', password='password4')
+        username='TestUser123',first_name='Test',last_name='Again', email='testing@aa.io', password='password4', profile_image='https://cdn-icons-png.flaticon.com/128/2919/2919906.png')
     eric = User(
-        username='Eric23',first_name='Eric',last_name='Hernandez', email='eric@aa.io', password='password5')
+        username='Eric23',first_name='Eric',last_name='Hernandez', email='eric@aa.io', password='password5', profile_image='https://cdn-icons-png.flaticon.com/128/219/219970.png')
     rebecca = User(
-        username='Rebecca2256',first_name='Rebecca',last_name='Smith', email='rebecca@aa.io', password='password6')
+        username='Rebecca2256',first_name='Rebecca',last_name='Smith', email='rebecca@aa.io', password='password6', profile_image='https://cdn-icons-png.flaticon.com/128/3135/3135823.png')
     hank = User(
-        username='Hank_Propane',first_name='Hank',last_name='Hill', email='hank@aa.io', password='password7')
+        username='Hank_Propane',first_name='Hank',last_name='Hill', email='hank@aa.io', password='password7', profile_image='https://cdn-icons-png.flaticon.com/128/206/206853.png')
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
