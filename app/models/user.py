f93355af17c920980bf7bc1ed29f5c4b687c727b
Brian8771 <brian8771@gmail.com>
2022-09-25 18:54:31 -0500

    added follow feature

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,15 @@
-
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from .caws import caw_likes
 from .comments import comment_likes
+
+
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
+)
 
 
 class User(db.Model, UserMixin):
@@ -37,6 +43,12 @@ class User(db.Model, UserMixin):
         cascade="all, delete"
     )
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     @property
     def password(self):
         return self.hashed_password
@@ -48,6 +60,24 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    def follow(self, users):
+        if not self.is_following(users):
+            self.followed.append(users)
+
+    def unfollow(self, users):
+        if self.is_following(users):
+            self.followed.remove(users)
+
+    def is_following(self, users):
+        return self.followed.filter(
+            followers.c.followed_id == users.id).count() > 0
+
+    # def followed_posts(self):
+    #     return Post.query.join(
+    #         followers, (followers.c.followed_id == Post.user_id)).filter(
+    #             followers.c.follower_id == self.id).order_by(
+    #                 Post.timestamp.desc())
+
     def to_dict(self):
         return {
             'id': self.id,
@@ -57,5 +87,8 @@ class User(db.Model, UserMixin):
             'lastName': self.last_name,
             'profileImage': self.profile_image,
             'headerImage': self.header_image,
-            'bio': self.bio
+            'bio': self.bio,
+            'followingCount': self.followed.count(),
+            'followerCount': self.followers.count(),
+            'followers': [followers.to_dict() for followers in self.followers],
         }
