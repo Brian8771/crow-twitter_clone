ba166b5e46fb52a1da852fcdcebc40aadc21a929
Brian8771 <brian8771@gmail.com>
2023-03-30 18:42:04 -0500

    making my code cleaner

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -12,6 +12,7 @@ import UserFollowersModal from './UserFollowersModal';
 import UserFollowingsModal from './UserFollowingsModal';
 import { getAllCommentsByUserId } from '../store/comments';
 import ClipLoader from "react-spinners/ClipLoader";
+import Caw from './Caw';
 
 
 function User() {
@@ -46,8 +47,6 @@ function User() {
     setRefresh(true)
   }
 
-  // console.log(getLikedCaws());
-
   const handleLikes = async (id) => {
     await dispatch(likeCawThunk(id))
     setLoaded(false)
@@ -55,6 +54,7 @@ function User() {
     setLoaded(true)
 
   }
+
 
   const ifFollows = () => {
     const follows = user.followers
@@ -102,6 +102,54 @@ function User() {
   const unfollowingUser = async () => {
     await dispatch(unfollowUser(user.username))
   }
+
+  const option1 = caws.length > 0 && isLoaded &&
+    caws.map(caw => {
+      return <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', borderBottom: '#2f3336 1px solid', padding: '10px 10px', borderLeft: 'black .5px solid', borderRight: 'black .5px solid', width: '100%' }}>
+        <div className='px-2.5 py-1.5 mr-3'>
+          <img className='h-12 w-12 rounded-full object-cover' src={caw.user.profileImage} alt='profilePic' />
+        </div>
+        <div className='test' style={{ flexDirection: 'column', alignItems: 'flex-start', width: '80%' }}>
+          <NavLink style={{ textDecoration: 'none' }} to={`/users/${caw.user.id}`}>
+
+            <p style={{ color: 'white' }} className='pTag'><span className='firstNameP'>{caw.user.firstName}</span> <span style={{ color: 'gray' }}>@{caw.user.username}</span><span style={{ marginLeft: '6px', color: 'gray' }}>{timeAfterCreated(caw)}</span></p>
+          </NavLink>
+          <NavLink style={{ textDecoration: 'none' }} to={`/caw/${caw.id}`}>
+            <p className='pTag' style={{ paddingTop: '10px', width: '100%', marginRight: '0', color: 'white' }} >{caw.caw}</p>
+            {caw.image && <img className='cawImage mb-2 aspect-square' src={caw.image} alt='image' />}
+          </NavLink>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+            <div className='likeButton' onClick={() => handleLikes(caw.id)} >
+              {caw.likeStatus === 1 ?
+                <div className='svgContainer'>
+                  <img src={likedIcon} alt="like-button-icon" className="like-button-icon" style={{ height: '16px', width: '16px', cursor: 'pointer' }} />
+                </div>
+                :
+                <div className='svgContainer'>
+                  <svg className='svgHeart' height="16px" viewBox="0 0 93 87" >
+                    <path d="M46.0625 86.6892H45.9983C34.1596 86.4692 0 55.605 0 26.3725C0 12.3292 11.5729 0 24.7637 0C35.2596 0 42.3179 7.24167 46.0579 12.5125C49.7887 7.25083 56.8471 0 67.3475 0C80.5475 0 92.1158 12.3292 92.1158 26.3771C92.1158 55.6004 57.9517 86.4646 46.1129 86.68H46.0625V86.6892ZM24.7683 6.87958C15.235 6.87958 6.87958 15.9912 6.87958 26.3817C6.87958 52.69 39.1187 79.53 46.0671 79.8142C53.0246 79.53 85.2546 52.6946 85.2546 26.3817C85.2546 15.9912 76.8992 6.87958 67.3658 6.87958C55.7792 6.87958 49.3075 20.3362 49.2525 20.4692C48.1983 23.045 43.9542 23.045 42.8954 20.4692C42.8312 20.3317 36.3642 6.87958 24.7729 6.87958H24.7683Z" />
+                  </svg>
+                </div>
+              }
+              {caw.likeStatus === 1 ?
+                <p style={{ marginLeft: '12px', cursor: 'pointer', color: '#f9197f' }}>{caw.totalLikes}</p> :
+                <p className='numberOfLikes'>{caw.totalLikes}</p>
+              }
+            </div>
+            <NavLink style={{ textDecoration: 'none' }} to={`/caw/${caw.id}`}>
+              <div className='commentButton' >
+                <div className='svgContainer'>
+                  <svg className='svgComment' height="20px" viewBox="0 0 140 140" >
+                    <path d="M81.935 13.0783L57.7383 13.02H57.7267C32.2117 13.02 12.2267 33.0108 12.2267 58.5317C12.2267 82.4367 30.8117 100.567 55.7725 101.523V123.853C55.7725 124.483 56.0292 125.522 56.4725 126.204C57.3008 127.517 58.7125 128.228 60.1592 128.228C60.9642 128.228 61.775 128.007 62.5042 127.54C64.0442 126.56 100.263 103.39 109.684 95.4217C120.779 86.03 127.418 72.2633 127.435 58.6017V58.5025C127.4 33.0283 107.427 13.0783 81.935 13.0725V13.0783ZM104.026 88.7483C97.4108 94.3483 75.6642 108.611 64.5225 115.833V97.2417C64.5225 94.8267 62.5683 92.8667 60.1475 92.8667H57.8375C36.4875 92.8667 20.9825 78.4233 20.9825 58.5317C20.9825 37.9167 37.1292 21.77 57.7325 21.77L81.9233 21.8283H81.935C102.538 21.8283 118.685 37.9633 118.697 58.555C118.679 69.6967 113.202 80.9783 104.032 88.7483H104.026Z" />
+                  </svg>
+                </div>
+                <p className='totalComments'>{caw.totalComments}</p>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    })
 
   useMemo(() => {
     setOption('Caws')
@@ -175,54 +223,7 @@ function User() {
             </div>}
             <div style={{ display: 'flex', flexDirection: 'column-reverse', width: '100%' }}>
 
-              {option === 'Caws' && caws.length > 0 && isLoaded &&
-                caws.map(caw => {
-                  return <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', borderBottom: '#2f3336 1px solid', padding: '10px 10px', borderLeft: 'black .5px solid', borderRight: 'black .5px solid', width: '100%' }}>
-                    <div className='px-2.5 py-1.5 mr-3'>
-                      <img className='h-12 w-12 rounded-full object-cover' src={caw.user.profileImage} alt='profilePic' />
-                    </div>
-                    <div className='test' style={{ flexDirection: 'column', alignItems: 'flex-start', width: '80%' }}>
-                      <NavLink style={{ textDecoration: 'none' }} to={`/users/${caw.user.id}`}>
-
-                        <p style={{ color: 'white' }} className='pTag'><span className='firstNameP'>{caw.user.firstName}</span> <span style={{ color: 'gray' }}>@{caw.user.username}</span><span style={{ marginLeft: '6px', color: 'gray' }}>{timeAfterCreated(caw)}</span></p>
-                      </NavLink>
-                      <NavLink style={{ textDecoration: 'none' }} to={`/caw/${caw.id}`}>
-                        <p className='pTag' style={{ paddingTop: '10px', width: '100%', marginRight: '0', color: 'white' }} >{caw.caw}</p>
-                        {caw.image && <img className='cawImage mb-2 aspect-square' src={caw.image} alt='image' />}
-                      </NavLink>
-                      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                        <div className='likeButton' onClick={() => handleLikes(caw.id)} >
-                          {caw.likeStatus === 1 ?
-                            <div className='svgContainer'>
-                              <img src={likedIcon} alt="like-button-icon" className="like-button-icon" style={{ height: '16px', width: '16px', cursor: 'pointer' }} />
-                            </div>
-                            :
-                            <div className='svgContainer'>
-                              <svg className='svgHeart' height="16px" viewBox="0 0 93 87" >
-                                <path d="M46.0625 86.6892H45.9983C34.1596 86.4692 0 55.605 0 26.3725C0 12.3292 11.5729 0 24.7637 0C35.2596 0 42.3179 7.24167 46.0579 12.5125C49.7887 7.25083 56.8471 0 67.3475 0C80.5475 0 92.1158 12.3292 92.1158 26.3771C92.1158 55.6004 57.9517 86.4646 46.1129 86.68H46.0625V86.6892ZM24.7683 6.87958C15.235 6.87958 6.87958 15.9912 6.87958 26.3817C6.87958 52.69 39.1187 79.53 46.0671 79.8142C53.0246 79.53 85.2546 52.6946 85.2546 26.3817C85.2546 15.9912 76.8992 6.87958 67.3658 6.87958C55.7792 6.87958 49.3075 20.3362 49.2525 20.4692C48.1983 23.045 43.9542 23.045 42.8954 20.4692C42.8312 20.3317 36.3642 6.87958 24.7729 6.87958H24.7683Z" />
-                              </svg>
-                            </div>
-                          }
-                          {caw.likeStatus === 1 ?
-                            <p style={{ marginLeft: '12px', cursor: 'pointer', color: '#f9197f' }}>{caw.totalLikes}</p> :
-                            <p className='numberOfLikes'>{caw.totalLikes}</p>
-                          }
-                        </div>
-                        <NavLink style={{ textDecoration: 'none' }} to={`/caw/${caw.id}`}>
-                          <div className='commentButton' >
-                            <div className='svgContainer'>
-                              <svg className='svgComment' height="20px" viewBox="0 0 140 140" >
-                                <path d="M81.935 13.0783L57.7383 13.02H57.7267C32.2117 13.02 12.2267 33.0108 12.2267 58.5317C12.2267 82.4367 30.8117 100.567 55.7725 101.523V123.853C55.7725 124.483 56.0292 125.522 56.4725 126.204C57.3008 127.517 58.7125 128.228 60.1592 128.228C60.9642 128.228 61.775 128.007 62.5042 127.54C64.0442 126.56 100.263 103.39 109.684 95.4217C120.779 86.03 127.418 72.2633 127.435 58.6017V58.5025C127.4 33.0283 107.427 13.0783 81.935 13.0725V13.0783ZM104.026 88.7483C97.4108 94.3483 75.6642 108.611 64.5225 115.833V97.2417C64.5225 94.8267 62.5683 92.8667 60.1475 92.8667H57.8375C36.4875 92.8667 20.9825 78.4233 20.9825 58.5317C20.9825 37.9167 37.1292 21.77 57.7325 21.77L81.9233 21.8283H81.935C102.538 21.8283 118.685 37.9633 118.697 58.555C118.679 69.6967 113.202 80.9783 104.032 88.7483H104.026Z" />
-                              </svg>
-                            </div>
-                            <p className='totalComments'>{caw.totalComments}</p>
-                          </div>
-                        </NavLink>
-                      </div>
-                    </div>
-
-                  </div>
-                })
+              {option === 'Caws' && option1
               }
               {option === 'Likes' && user.like_caws.length > 0 && isLoaded &&
                 getLikedCaws().map(caw => {
